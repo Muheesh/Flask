3018b3cd16417cb93395fb383a489c276690245f
Muheesh <muheeshas@gmail.com>
2022-04-04 10:10:59 +0530

    home

--- App.py
+++ App.py
@@ -4,9 +4,15 @@ app = Flask(__name__)
 @app.route('/')
 def welcome():
     return "Welcome to my website"
+
 @app.route('/contact')
 def Contact_page():
     return "This is contact page"
+
+@app.route('/home')
+def Home_page():
+    return "This is home page"
+
 
 if __name__=="__main__":
     app.run()
